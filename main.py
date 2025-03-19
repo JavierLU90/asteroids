0a85a0e446ed31d927e44f1164178010ce9bd61b
JavierLU90 <jlomelinu@gmail.com>
2025-03-19 14:11:26 -0600

    Detect asteroid collisions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame # type: ignore
 from constants import *
 from player import Player
@@ -28,6 +29,11 @@ def main():
                 return
     
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
